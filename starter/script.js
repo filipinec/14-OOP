50367992e7e32712c1821c8fb1d426fabe9fad54
Filip Tasevski <135996963+filipinec@users.noreply.github.com>
2024-01-02 11:23:03 +0100

    Update script.js

--- starter/script.js
+++ starter/script.js
@@ -1,6 +1,12 @@
 'use strict';
-const Person = function (firstName, birthYear) {};
-new Person('Filip', 1995);
+const Person = function (firstName, birthYear) {
+  this.myName = firstName;
+  this.yearOfBirth = birthYear;
+};
+const filip = new Person('Filip', 1995);
+console.log(filip);
 
 // 1. New {} is created
-// 2/
+// 2. Function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
